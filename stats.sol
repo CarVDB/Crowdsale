a2c0759bbaf6c1e5021dc3e4bc1082cfa658e18f
CarVDB <36619714+CarVDB@users.noreply.github.com>
2018-03-11 11:27:47 +0200

    Create stats.sol

---
+++ stats.sol
@@ -0,0 +1,51 @@
+pragma solidity ^0.4.18;
+
+contract Ownable {
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  function Ownable() public {
+    owner = msg.sender;
+  }
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+}
+
+contract Stats is Ownable {
+    bool public isPaused;
+    bool public isExtended;
+    bool public finished;
+    bool public softcapReached;
+    uint8 public currentWave;
+    uint256 public refundPrice;
+    uint256 public extendedTime;
+    address public preIcoWallet;
+
+    function monitor(bool _isPaused, bool _isExtended, bool _finished, bool _softCapReached, 
+                    uint8 _currentWave, uint256 _refundPrice, uint256 _extendedTime, 
+                    address _preIcoWallet) public 
+                    {
+        isPaused = _isPaused;
+        isExtended = _isExtended;
+        finished = _finished;
+        softcapReached = _softCapReached;
+        currentWave = _currentWave;
+        refundPrice = _refundPrice;
+        extendedTime = _extendedTime;
+        preIcoWallet = _preIcoWallet;
+    }
+
+    function terminateContract() onlyOwner external {
+        selfdestruct(owner);
+    }
+
+}
